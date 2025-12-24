baa7efa3633d2fbe9737e9829c038d58646dd07b
Sanjeet Dasharath <sanjeetdasharath@gmail.com>
2025-12-24 21:03:23 +1100

    implement einsum v1 for 2 tensors

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 use std::{error::Error, fs};
-use tensors::{Ax, Tensor, dims};
+use tensors::{Ax, EinsumResult, Tensor, dims, einsum2};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let data: Vec<f32> = fs::read_to_string("src/data.txt")?
-        .split(',')
-        .map(|s| s.trim().parse())
-        .collect::<Result<Vec<_>, _>>()?;
+    // let data: Vec<f32> = fs::read_to_string("src/data.txt")?
+    //     .split(',')
+    //     .map(|s| s.trim().parse())
+    //     .collect::<Result<Vec<_>, _>>()?;
 
-    let x: Tensor<f32> = Tensor::from_data(&data, &[3, 3, 2]);
+    // let x: Tensor<f32> = Tensor::from_data(&data, &[3, 3, 2]);
 
-    let y: Tensor<f32> = Tensor::from_data(&vec![100.0, 200.0], &[2]);
+    // let y: Tensor<f32> = Tensor::from_data(&vec![100.0, 200.0], &[2]);
 
-    let z = x.div(&y);
+    // let z = x.div(&y);
 
-    println!(
-        "x.shape = {:?}, y.shape = {:?}, z.shape = {:?}",
-        x.shape(),
-        y.shape(),
-        z.shape()
-    );
+    // println!(
+    //     "x.shape = {:?}, y.shape = {:?}, z.shape = {:?}",
+    //     x.shape(),
+    //     y.shape(),
+    //     z.shape()
+    // );
 
-    println!("{}", z);
+    // println!("{}", z);
+
+    let x: Tensor<f32> = Tensor::<f32>::randn(&[3, 2]);
+    let y: Tensor<f32> = Tensor::<f32>::randn(&[2, 3]);
+
+    let z = einsum2("ij,jk", &x, &y);
+
+    match z {
+        EinsumResult::Scalar(x) => println!("{}", x),
+        EinsumResult::Tensor(tensor) => println!("{}", tensor),
+    }
 
     Ok(())
 }
